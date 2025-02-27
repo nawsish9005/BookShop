ce0b732db1aa5e1398b914078b755930a3f574ed
nawsish <nawsish2018@gmail.com>
2025-02-27 14:13:31 +0600

    add home page

--- BookShop/obj/Debug/net8.0/BookShop.AssemblyInfo.cs
+++ BookShop/obj/Debug/net8.0/BookShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12141c851f9f6fdf13822be78bd2885cbc193c76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c061f0cbc4131c0de364571d1347f0b42b955a21")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
