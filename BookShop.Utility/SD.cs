5a3f3b5198c99f7e9d49bd567c7e7971f572a34d
nawsish <nawsish2018@gmail.com>
2025-03-05 15:04:37 +0600

    add utility

--- BookShop.Utility/SD.cs
+++ BookShop.Utility/SD.cs
@@ -10,7 +10,7 @@ namespace BookShop.Utility
     {
         //public const string Role_Customer = "Customer";
         //public const string Role_Company = "Company";
-        //public const string Role_Admin = "Admin";
+        public const string Role_Admin = "Admin";
         //public const string Role_Employee = "Employee";
 
         //public const string StatusPending = "Pending";
