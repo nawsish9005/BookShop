cb071626dbee3673dbb6c6b3259b0d0b8402bea9
nawsish <nawsish2018@gmail.com>
2025-03-07 16:08:00 +0600

    add models

--- BookShop.Models/obj/Debug/net8.0/BookShop.Models.AssemblyInfo.cs
+++ BookShop.Models/obj/Debug/net8.0/BookShop.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookShop.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a3f3b5198c99f7e9d49bd567c7e7971f572a34d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36fbf77752469a9b83023ada60cc39dccc2e3387")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookShop.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookShop.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
